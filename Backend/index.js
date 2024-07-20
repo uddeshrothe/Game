729d90f0085ebb30725dca50e53f79c04c16ebfd
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-20 13:12:55 +0530

    "Updated Backend code:  modified index.js to use dotenv and RAWG API key, removed fetchFromAPI function."

--- Backend/index.js
+++ Backend/index.js
@@ -1,61 +1,36 @@
 const express = require('express');
 const app = express()
-const cors = require('cors') 
+const cors = require('cors')
 const axios = require('axios')
+require('dotenv').config()
 
 // Define CORS options
 const corsOptions = {
-    origin: '*', 
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-  };
+};
 
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json())
 
-const BASE_URL = "https://www.freetogame.com/api"
+const RAWG_API_KEY = process.env.RAWG_API_KEY;
 
-//Helper function for the api
-const fetchFromAPI = async (endpoint) => {
-    try {
-        const response = await axios.get(`${BASE_URL}${endpoint}`)
-        return response.data
-    } catch (error) {
-        console.error(`Error fetching data from ${endpoint}:`, error.message);
-        throw error;
-    }
-}
-
-// Route to fetch all games
+// Endpoint to get a list of games
 app.get('/games', async (req, res) => {
     try {
-        const games = await fetchFromAPI('/games');
-        res.json(games);
+        const response = await axios.get('https://api.rawg.io/api/games', {
+            params: {
+                key: RAWG_API_KEY,
+            },
+          });
+            // Extract names from the response
+    //const gameNames = response.data.results.map(game => game.name);
+    res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'Error retrieving games' });
-    }
-});
-
-// Route to fetch a single game
-app.get('/game', async (req, res) => {
-    const { id } = req.query
-    try {
-        const game = await fetchFromAPI(`/game?id=${id}`)
-        res.status(200).json(game)
-    } catch (error) {
-        res.status(500).json({error: 'Error retrieving game'})
-    }
-})
-
-// Get games as per the category
-app.get('/games/category', async (req, res) => {
-    const { category } = req.query;
-    try {
-        const games = await fetchFromAPI(`/games?category=${category}`);
-        res.json(games);
-    } catch (error) {
-        res.status(500).json({ error: 'Error retrieving games by category' });
+        console.error('Error fetching games:', error);
+        res.status(500).json({ error: 'Error fetching games' });
     }
 });
 
