5fbd01fd49b32e990ae8aa7e1903fc00a32026ea
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-20 13:16:49 +0530

    "Added new API endpoint for fetching genres and refactored existing games endpoint"

--- Backend/index.js
+++ Backend/index.js
@@ -24,15 +24,30 @@ app.get('/games', async (req, res) => {
             params: {
                 key: RAWG_API_KEY,
             },
-          });
-            // Extract names from the response
-    //const gameNames = response.data.results.map(game => game.name);
-    res.json(response.data);
+        });
+        // Extract names from the response
+        //const gameNames = response.data.results.map(game => game.name);
+        res.json(response.data);
     } catch (error) {
         console.error('Error fetching games:', error);
         res.status(500).json({ error: 'Error fetching games' });
     }
 });
+
+app.get('/genres', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.rawg.io/api/genres', {
+            params: {
+                key: RAWG_API_KEY,
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching genres:', error);
+        res.status(500).json({ error: 'Error fetching genres' });
+    }
+});
+
 
 app.listen(5000, () => {
     console.log("Server is running!")
